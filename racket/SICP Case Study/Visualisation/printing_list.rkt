3e7738a694ab6b34faaba7a4ab1e57a918062cc1
Mithun Rosinth <44864907+mivinromo2011@users.noreply.github.com>
2021-11-22 21:17:47 +0530

    sicp case study

--- racket/SICP Case Study/Visualisation/printing_list.rkt
+++ racket/SICP Case Study/Visualisation/printing_list.rkt
@@ -3,11 +3,12 @@
 (require 2htdp/image)
 
 (define ele_box (square 100 "outline" "green"))
-;(beside ele_box ele_box)
-;(above ele_box ele_box)
-;(above ele_box (rotate 45 ele_box))
-(define arr '(1 2 3 5))
 
-(overlay (text (number->string (car arr)) 40 "red") ele_box)
-(arr (cdr arr))
-(overlay (text (number->string (car arr)) 40 "red") ele_box)
+(beside ele_box ele_box ele_box)
+;(define arr '(1 2 3 5))
+
+;(define (list_print lst)
+  ;(if (empty? lst)
+      ;'()
+      ; ((overlay (text (number->string (car arr)) 40 "red") ele_box)(list_print (cdr arr)))))
+;(list_print arr)
